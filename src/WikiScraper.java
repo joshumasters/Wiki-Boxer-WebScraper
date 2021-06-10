b0928a8a3d8ec0e79727b2987a38a9483cc0b6c0
Joshua Masters <joshmasters724@gmail.com>
2021-06-10 12:56:37 -0400

    Added File writer to create unique spreadsheets

--- src/WikiScraper.java
+++ src/WikiScraper.java
@@ -1,11 +1,13 @@
 
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -21,27 +23,44 @@ public class WikiScraper {
         try {
             final Document doc = Jsoup.connect(url).get();
             final Elements rows = doc.select("table.wikitable tr");
+            final String title = doc.select("title").text();
             for (Element row : rows) {
                 Elements items = row.select("td");
                 if(items.size() == 9){
                     FightEntry entry = new FightEntry(items.get(0).text(), items.get(1).text(), items.get(2).text(), items.get(3).text(), items.get(4).text(), items.get(5).text(), items.get(6).text(), items.get(7).text(), items.get(8).text());
-                    System.out.println(entry.toString());
                     entries.add(entry);
                 }
             }
+            Workbook book = new XSSFWorkbook();
+            Sheet sheet = book.createSheet("FightEntries");
+            String[] headings = {"Fight Number", "Fight Result", "Current Record", "Opponent", "Result Type", "Round/Time", "Date", "Location", "Notes"};
+            Row header = sheet.createRow(0);
+            for (int i = 0; i < headings.length; i++) {
+                Cell cell = header.createCell(i);
+                cell.setCellValue(headings[i]);
+            }
+            int rowNum = 1;
+            for(FightEntry entry : entries){
+                Row row = sheet.createRow(rowNum++);
+                row.createCell(0).setCellValue(entry.getFightNum());
+                row.createCell(1).setCellValue(entry.getRecord());
+                row.createCell(2).setCellValue(entry.getResult());
+                row.createCell(3).setCellValue(entry.getOpponent());
+                row.createCell(4).setCellValue(entry.getResultType());
+                row.createCell(5).setCellValue(entry.getRoundTime());
+                row.createCell(6).setCellValue(entry.getDate());
+                row.createCell(7).setCellValue(entry.getLocation());
+                row.createCell(8).setCellValue(entry.getNotes());
+            }
+            sheet.createFreezePane(0, 1);
+            for (int i = 0; i < headings.length; i++) {
+                sheet.autoSizeColumn(i);
+            }
+            FileOutputStream fileOut = new FileOutputStream("/Users/joshuamasters724/Documents/Programming/Spreadsheets/" + title + ".xlsx");
+            book.write(fileOut);
+            fileOut.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        try {
-			FileOutputStream fileOut = new FileOutputStream(
-					"/Users/joshuamasters724/Documents/Programming/Test.xlsx");
-			ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
-			objectOut.writeObject(entries);
-			objectOut.close();
-		} catch (Exception e) {
-            e.printStackTrace();
-			return;
-		}
     }
 }
